eaf991f7b9cd603076550ce21d9b10d6af599096
Robiwizzard <hortobagyi.robert@gmail.com>
2022-11-02 22:57:19 +0100

    Math fix again lmao

--- EngravingBook.py
+++ EngravingBook.py
@@ -4,15 +4,15 @@ def calc():
     print("Input number of players:")
     players = float(input())
     print("Input engraving book price:")
-    price = float(input())
-    bid = (price * 0.95 - (price * 0.95 / players)) / 1.1
+    price = float(input()) * 0.95
+    bid =  (price - (price - ((players - 1) * (price / players)))) / 1.1
     print(
     "Bid for maximum profit: " +
     str(math.ceil(bid))
     )
     print(
     "Bid for equal profit: " +
-    str(math.ceil((price * 0.95 - (price * 0.95 / players))))
+    str(math.ceil(price - (price - ((players - 1) * (price / players)))))
     )
     
 calc()
